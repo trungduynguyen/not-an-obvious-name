908a0684590a4e9050e99f60bfd12f883e4b8b22
trungduynguyen <duy.nguyen2015@ict.jvn.edu.vn>
2018-03-03 14:14:17 +0100

    Add argument parser

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -8,18 +8,24 @@ import os
 import sqlite3
 import datetime as dt
 import pandas as pd
+import argparse
+import logging
 
-class Bitcoin_price:
+class BitcoinPrice:
+    
+    url = """https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={0}&market={1}&apikey={2}"""
+    api_key = ""
+    db_name = ""
+    selected_cols = ['1b. open (USD)','2b. high (USD)','3b. low (USD)','4b. close (USD)','5. volume', '6. market cap (USD)']
+
+    
     
     def __init__(self,api_key,db_name,table_name):
         
-        #self.api_key= "SCIR7S6G9T0P0UW1"
         self.api_key= api_key
-        self.selected_cols = ['1b. open (USD)','2b. high (USD)','3b. low (USD)','4b. close (USD)','5. volume', '6. market cap (USD)']
         self.db_name = db_name
         self.table_name = table_name
-        self.conn = sqlite3.connect(self.db_name)
-        
+
     def close_db(self):
         self.conn.close()
     
@@ -29,10 +35,10 @@ class Bitcoin_price:
         Get bitcoin prices from alphavantage API and save to csv file
         """
         
-        url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol="+str(symbol)+"&market="+str(market)+"&apikey="+self.api_key
-        response= requests.get(url)
+        response= requests.get(self.url.format(symbol,market,self.api_key))
         
         json_response = response.json()
+
         df = pd.DataFrame(json_response["Time Series (Digital Currency Daily)"]).T[self.selected_cols]
         df['Date'] = pd.to_datetime(df.index)   #Transform Date column to datetime datatype
         df.set_index('Date', inplace=True)
@@ -42,7 +48,7 @@ class Bitcoin_price:
         df['year_week'] = df.index.to_series().apply(lambda x: dt.datetime.strftime(x,'%Y-week(%U)'))   #Get week of the date
         
         df.to_csv('./data/daily_data.csv',sep=',')
-        print("SUCCESSFULLY Collected and Saved data to 'daily_data.csv' !\n")
+
         
     
 ######################################################################################
@@ -57,10 +63,10 @@ class Bitcoin_price:
         df_report['weekly_max'] = (df_daily.groupby(['year_week']).max())['close']  #Get maximum price in each week
         df_report['weekly_min'] = (df_daily.groupby(['year_week']).min())['close']  #Get minimum price in each week
         df_report.to_csv('./data/pandas_weekly_report.csv')
-        print("SUCCESSFULLY Saved weekly data to 'pandas_weekly_report.csv' !\n")
+
         return df_report
     
-    
+    @staticmethod
     def get_relative_span(self,x):
         """
         Apply mathematical formula to get weekly relative span
@@ -83,11 +89,13 @@ class Bitcoin_price:
         Create sqlite table and insert data from pandas DataFrame to table
         """
         
-        cur= self.conn.cursor()
-        cur.execute('''DROP TABLE IF EXISTS %s;'''%(self.table_name)) #Drop table if exists
+        try:
+            conn = sqlite3.connect(self.db_name)
+            cur= conn.cursor()
+            cur.execute('''DROP TABLE IF EXISTS %s;'''%(self.table_name)) #Drop table if exists
         
-        # Create new table and insert daily data
-        cur.execute('''CREATE TABLE %s ( date DATE PRIMARY KEY , 
+            # Create new table and insert daily data
+            cur.execute('''CREATE TABLE %s ( date DATE PRIMARY KEY , 
                                          open FLOAT, 
                                          high FLOAT, 
                                          low FLOAT,
@@ -96,39 +104,36 @@ class Bitcoin_price:
                                          market_cap FLOAT,
                                          year_week VARCHAR(15));'''%(self.table_name))
     
-        daily_df = pd.read_csv('./data/daily_data.csv')
-        daily_df['Date'] = pd.to_datetime(daily_df['Date'])
-        daily_df.set_index('Date', inplace=True)
-        
-        try:
-            daily_df.to_sql(self.table_name, self.conn, if_exists='replace')
-            print("Successfully Inserted into DB!\n")
+            daily_df = pd.read_csv('./data/daily_data.csv')
+            daily_df['Date'] = pd.to_datetime(daily_df['Date'])
+            daily_df.set_index('Date', inplace=True)
+            daily_df.to_sql(self.table_name,conn, if_exists='replace')
+            conn.commit()
         except Exception as e:
             print("Error!")
         finally:
-            self.conn.commit()
-            #conn.close()
+            conn.close()
     
 
     def get_weekly_stats_query(self):
         
+        conn = sqlite3.connect(self.db_name)
         df = pd.read_sql_query('''select year_week, AVG(close) weekly_average,
                                   MAX(close) weekly_max,
                                   MIN(close) weekly_min
                                   from %s
-                                  group by year_week'''%(self.table_name),self.conn)
+                                  group by year_week'''%(self.table_name),conn)
         df.to_csv('./data/sql_weekly_report.csv')
-        print("SUCCESSFULLY Saved weekly data to 'sql_weekly_report.csv' !\n")
         
     def get_relative_span_query(self):
         
+        conn = sqlite3.connect(self.db_name)
         df = pd.read_sql_query('''select year_week ,MAX((weekly_max-weekly_min)/weekly_min) relative_span from (
                                   select year_week,
                                   MAX(close) weekly_max,
                                   MIN(close) weekly_min
                                      from %s
-                                     group by year_week)'''%(self.table_name), self.conn)
-        print("Maximum relative span by SQL query:\n")
+                                     group by year_week)'''%(self.table_name), conn)
         print(df)
         
         
@@ -136,35 +141,48 @@ class Bitcoin_price:
 ##########################  MAIN FUNCTION      ##########################################        
 def main():
     
-  os.chdir(os.getcwd())
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('api_key', action="store", type=str)
+    parser.add_argument('db_name', action="store", type=str)
+    parser.add_argument('table_name', action="store", type=str)
+    args = parser.parse_args()
+    
+    #logger = logging.getLogger(__name__)
+    logging.info('Starting process')
+    logging.basicConfig(level=logging.INFO)
+    os.chdir(os.getcwd())
   
-  #Inititate new instance
-  Bitcoin = Bitcoin_price("SCIR7S6G9T0P0UW1","./db/crypto.db","bitcoin")
-  
-  # Get Bitcoin daily prices via API and store to daily_data.csv file
-  Bitcoin.get_digital_currency_daily()
-  
-  # Create sqlite table to store data
-  Bitcoin.create_table()
-  
-  
-  
-  daily_df = pd.read_csv('./data/daily_data.csv',usecols=['close','year_week']) # Load from csv file
-  daily_df['close'] = daily_df['close'].astype(float)
-  
-  #Compute method 1: Python Memory
-  weekly_df = Bitcoin.get_weekly_stats_pandas(daily_df)
-  Bitcoin.get_relative_span_pandas(weekly_df)
-  
-  
-  #Compute method 2: SQL Queries
-  Bitcoin.get_weekly_stats_query() 
-  Bitcoin.get_relative_span_query()
-  
-  Bitcoin.close_db()
-  
-  print("FINISH PROGRAM!")
+    #Inititate new instance
+    Bitcoin = BitcoinPrice(args.api_key,args.db_name,args.table_name)
+    
+    # Get Bitcoin daily prices via API and store to daily_data.csv file
+    Bitcoin.get_digital_currency_daily()
+#  
+#    # Create sqlite table to store data
+#    Bitcoin.create_table()
+#  
+#  
+#  
+    daily_df = pd.read_csv('./data/daily_data.csv',usecols=['close','year_week']) # Load from csv file
+    daily_df['close'] = daily_df['close'].astype(float)
+#  
+    #Compute method 1: Python Memory
+    weekly_df = Bitcoin.get_weekly_stats_pandas(daily_df)
+    print(len(weekly_df))
+#    Bitcoin.get_relative_span_pandas(weekly_df)
+#  
+#  
+    #Compute method 2: SQL Queries
+    #Bitcoin.get_weekly_stats_query() 
+    #Bitcoin.get_relative_span_query()
+#  
+#    Bitcoin.close_db()
+#  
+#    print("FINISH PROGRAM!")
 
 if __name__== "__main__":
-  main()      
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',datefmt='%m/%d/%Y %I:%M:%S',level=logging.INFO)
+    main()      
 
