29b36f53aa60c41078ea55975a4d79964e7b9a95
trungduynguyen <duy.nguyen2015@ict.jvn.edu.vn>
2018-03-04 00:12:22 +0100

    Modify method of getting week of year in queries

--- query_lib.py
+++ query_lib.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar  3 22:19:40 2018
-
 @author: Trung Duy
 """
 
@@ -15,6 +13,16 @@ create_query = """CREATE TABLE {0} ( date DATE PRIMARY KEY ,
                                      volume FLOAT, 
                                      market_cap FLOAT);"""
 
-weekly_price_query = """"""
+weekly_price_query = """select strftime('%Y-week(%W)',date) as year_week, AVG(close) weekly_average,
+                               MAX(close) weekly_max,
+                               MIN(close) weekly_min
+                        from {0}
+                        group by year_week;"""
 
-max_relative_span_query = """"""
+max_relative_span_query = """select year_week ,MAX((weekly_max-weekly_min)/weekly_min) relative_span 
+                            from (
+                                    select strftime('%Y-week(%W)',date) as year_week,
+                                           MAX(close) weekly_max,
+                                           MIN(close) weekly_min
+                                    from {0}
+                                    group by year_week)"""
