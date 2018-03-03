8f4c43134ff9d03cbb77dad07997a9b5a28a91e2
trungduynguyen <duy.nguyen2015@ict.jvn.edu.vn>
2018-03-04 00:14:09 +0100

    Capitalize constant variable names

--- query_lib.py
+++ query_lib.py
@@ -3,9 +3,9 @@
 @author: Trung Duy
 """
 
-drop_query = """DROP TABLE IF EXISTS {0};"""
+DROP_QUERY = """DROP TABLE IF EXISTS {0};"""
 
-create_query = """CREATE TABLE {0} ( date DATE PRIMARY KEY , 
+CREATE_QUERY = """CREATE TABLE {0} ( date DATE PRIMARY KEY , 
                                      open FLOAT, 
                                      high FLOAT, 
                                      low FLOAT,
@@ -13,14 +13,14 @@ create_query = """CREATE TABLE {0} ( date DATE PRIMARY KEY ,
                                      volume FLOAT, 
                                      market_cap FLOAT);"""
 
-weekly_price_query = """select strftime('%Y-week(%W)',date) as year_week, AVG(close) weekly_average,
+WEEKLY_PRICE_QUERY = """select strftime('%Y-week(%W)',date) as year_week, AVG(close) weekly_average,
                                MAX(close) weekly_max,
                                MIN(close) weekly_min
                         from {0}
                         group by year_week;"""
 
-max_relative_span_query = """select year_week ,MAX((weekly_max-weekly_min)/weekly_min) relative_span 
-                            from (
+MAX_RELATIVE_SPAN_QUERY = """select year_week ,MAX((weekly_max-weekly_min)/weekly_min) relative_span 
+                             from (
                                     select strftime('%Y-week(%W)',date) as year_week,
                                            MAX(close) weekly_max,
                                            MIN(close) weekly_min
