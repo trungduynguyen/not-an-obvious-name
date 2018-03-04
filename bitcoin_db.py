14040d6510d8306ed0eb29ab6fd050d9e1d02a96
trungduynguyen <duy.nguyen2015@ict.jvn.edu.vn>
2018-03-04 07:41:32 +0100

    Add __enter__ and __exit__, mofify create table()

--- bitcoin_db.py
+++ bitcoin_db.py
@@ -24,8 +24,11 @@ class BitcoinPriceDB(BitcoinPrice):
         self.conn = sqlite3.connect(self.db_name)
         self.cursor = self.conn.cursor()
         self.get_digital_currency_daily(save= False)
+        
+    def __enter__(self):
+        return self
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         if self.conn:
             self.conn.close()
                       
@@ -38,7 +41,6 @@ class BitcoinPriceDB(BitcoinPrice):
         else:
             return df
     
-
     def create_table(self):
         """
         Create sqlite table and insert data from pandas DataFrame to table
@@ -61,6 +63,7 @@ class BitcoinPriceDB(BitcoinPrice):
     def get_weekly_stats(self):
         
         df = self.query(ql.WEEKLY_PRICE_QUERY.format(self.table_name))
+        df.set_index('year_week', inplace=True)
         df.to_csv('./data/sql_weekly_report.csv')
         logging.info("Saved weekly data to 'sql_weekly_report.csv' !")
         
@@ -69,8 +72,8 @@ class BitcoinPriceDB(BitcoinPrice):
     def get_max_relative_span(self):
         
         df = self.query(ql.MAX_RELATIVE_SPAN_QUERY.format(self.table_name))
-        print("Maximum relative span by SQL query:\n")
-        print(df)
+        df.set_index('year_week', inplace=True)
+        print("Maximum relative span by SQL query:\n",df)
         
         return df
     
