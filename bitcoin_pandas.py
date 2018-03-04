8ebfe2be750718a0b63393f84ca2228cbda5c735
trungduynguyen <duy.nguyen2015@ict.jvn.edu.vn>
2018-03-04 07:43:58 +0100

    Import method to get daily_df in weekly_report_stats()

--- bitcoin_pandas.py
+++ bitcoin_pandas.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-
 @author: Trung Duy
 """
 
@@ -12,17 +11,17 @@ class BitcoinPricePandas(BitcoinPrice):
     
     def __init__(self,api_key):
         super().__init__(api_key)
-
-    
+   
     def get_weekly_stats(self):
         """
         Get weekly statistical measurements mean, max, min by pandas dataframe
         """
-        print(self.daily_df.head())
-        df_report = pd.DataFrame(index = self.daily_df['year_week'].unique() )
-        df_report['weekly_average'] = (self.daily_df.groupby(['year_week']).mean())['close'] #Get average price in each week
-        df_report['weekly_max'] = (self.daily_df.groupby(['year_week']).max())['close']  #Get maximum price in each week
-        df_report['weekly_min'] = (self.daily_df.groupby(['year_week']).min())['close']  #Get minimum price in each week
+        daily_df = self.get_daily_df(['close','year_week'])
+        
+        df_report = pd.DataFrame(index = daily_df['year_week'].unique() )
+        df_report['weekly_average'] = (daily_df.groupby(['year_week']).mean())['close'] #Get average price in each week
+        df_report['weekly_max'] = (daily_df.groupby(['year_week']).max())['close']  #Get maximum price in each week
+        df_report['weekly_min'] = (daily_df.groupby(['year_week']).min())['close']  #Get minimum price in each week
         df_report.to_csv('./data/pandas_weekly_report.csv')
         logging.info("Save data to pandas_weekly_report.csv")
         return df_report
@@ -38,14 +37,15 @@ class BitcoinPricePandas(BitcoinPrice):
         """
         Get maximum relative span by pandas dataframe
         """
-        print(df_weekly.head())
         result = df_weekly.apply(self.relative_span,axis=1) #Apply relative_span for each row
         df_span = pd.DataFrame(dict(relative_span=result),index =df_weekly.index)
-        print("Maximum relative span by Pandas Dataframe:")
-        print(df_span[df_span['relative_span'] == df_span['relative_span'].max() ])  
-    
+        res = df_span[df_span['relative_span'] == df_span['relative_span'].max() ]
+        res.index.name = 'year_week'
+        print("Maximum relative span by Pandas Dataframe:\n",res)
+        return res 
+        
     def process(self):
         
         weekly_df = self.get_weekly_stats()
-        self.get_max_relative_span(weekly_df)
+        max_span = self.get_max_relative_span(weekly_df)
         
